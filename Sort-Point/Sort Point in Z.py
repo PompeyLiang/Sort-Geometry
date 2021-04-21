bc1feb1f1b828d394f98c8678868f5a83c366283
PompeyLiang <38983015+PompeyLiang@users.noreply.github.com>
2021-04-21 22:57:17 +0200

    Update Sort Point in Z.py

--- Sort-Point/Sort Point in Z.py
+++ Sort-Point/Sort Point in Z.py
@@ -1,14 +1,27 @@
-'''
-This is a python component that sort input points based on Z axis  
-'''
-
-__author__ = "jujud"
+  '''
+This is a python component that sort input points based on points' Z coordinates.  
+Compent name: Sort Point in Z
+Input Parameter:
+        Geometry(Type:Point3d): The points are needed to sort.
+        Reverse(Type:Boolen): Whether reverse the output list.
+Output:
+        Geometey: Sorted points
+        
+__author__ = "Liang Mayuqi"
 __version__ = "2021.04.20"
-                
-import rhinoscriptsyntax as rs
+'''     
+  
 import Rhino.Geometry as rg
                 
 def sort_list_object(list):
+    '''
+  __name__: sort_list_object
+  __msg__: Sort one index list based on point.Z list; and then access points list with sorted index list. 
+  __paras__: 
+        geometry: Points that needed to sorted in Z Axis. 
+  __return__: 
+        curve list: Sorted points list.
+  '''
     length = len(list)
     index_list = [i for i in range(length)]
     Z_value = [point.Z for point in list]
