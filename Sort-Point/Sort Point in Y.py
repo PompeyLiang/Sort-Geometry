b9812ee99e35d8baf9b513a00e61ef580c237a4d
PompeyLiang <38983015+PompeyLiang@users.noreply.github.com>
2021-04-21 22:55:50 +0200

    Update Sort Point in Y.py

--- Sort-Point/Sort Point in Y.py
+++ Sort-Point/Sort Point in Y.py
@@ -1,15 +1,28 @@
 '''
-    This is a python component that sort input points based on Y axis  
-'''
-
-__author__ = "jujud"
+This is a python component that sort input points based on points' Y coordinates.  
+Compent name: Sort Point in Y
+Input Parameter:
+        Geometry(Type:Point3d): The points are needed to sort.
+        Reverse(Type:Boolen): Whether reverse the output list.
+Output:
+        Geometey: Sorted points
+        
+__author__ = "Liang Mayuqi"
 __version__ = "2021.04.20"
-                
-import rhinoscriptsyntax as rs
+'''     
+
 import Rhino.Geometry as rg
                 
                 
 def sort_list_object(list):
+  '''
+  __name__: sort_list_object
+  __msg__: Sort one index list based on point.Y list; and then access points list with sorted index list. 
+  __paras__: 
+        geometry: Points that needed to sorted in Y Axis. 
+  __return__: 
+        curve list: Sorted points list.
+  '''
     length = len(list)
     index_list = [i for i in range(length)]
     Y_value = [point.Y for point in list]
