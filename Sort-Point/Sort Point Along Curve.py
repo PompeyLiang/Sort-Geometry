ef234450d16b62f0e5a1bc0cd4e85f468ab6c590
PompeyLiang <38983015+PompeyLiang@users.noreply.github.com>
2021-04-23 10:17:30 +0200

    Update Sort Point Along Curve.py

--- Sort-Point/Sort Point Along Curve.py
+++ Sort-Point/Sort Point Along Curve.py
@@ -1,14 +1,34 @@
 '''
-This is a python component that sort input points based on input curve  
-'''
 
-__author__ = "Liang_Mayuqi"
+This is a python component that sort input points based on guide curve. Can choose two different ways to sort input points: to sort points by the distance between points 
+to the cloest points on guide curve; or sort points along guide curve tangent. 
+Compent name: Sort Point Along Curve
+Input Parameter:
+        Geometry(Type:Point3d): The points are needed to sort.
+        Curve(Type:Curve): Sort points based on this curve. 
+        Distance(Type: Boolen): Whether sort input points based on distance between points to the cloesest point on guide curve.
+        Reverse(Type:Boolen): Whether reverse the output list.
+Output:
+        Geometey: Sorted points
+        
+__author__ = "Liang Mayuqi"
 __version__ = "2021.04.20"
-                                                
-import rhinoscriptsyntax as rs
+
+'''     
+
 import Rhino.Geometry as rg
         
 def sort_list_object(list, curve, boolen):
+    '''
+  __name__: sort_list_object
+  __msg__: Sort index list based on t values of the cloesest points on guide curve correspoinding to the input points; and then access input points list with sorted index list. 
+  __paras__: 
+        geometry: Points that needed to sorted.
+        curve: The guide curve to sort index values.
+        boolen: Whether sort curves based on distance.
+  __return__: 
+        index list: Sorted index list.
+  '''
     length = len(list)
     t_value = [curve.ClosestPoint(point)[1] for point in list]
     index_list = [i for i in range(length)]
